47cf53f09711be2f964b93fde014d775cf2f1532
Nicola Brisotto <nicolabrisotto@gmail.com>
2023-09-18 19:06:23 +0200

    init

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.poetry2nix.mkPoetryApplication {
+  projectDir = ./.;
+  overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend
+    (self: super: {
+      werkzeug = super.werkzeug.overridePythonAttrs
+      (
+        old: {
+          buildInputs = (old.buildInputs or [ ]) ++ [ super.flit-core ];
+        }
+      );
+      flask = super.flask.overridePythonAttrs
+      (
+        old: {
+          buildInputs = (old.buildInputs or [ ]) ++ [super.setuptools] ++ [super.flit-core];
+        }
+      );
+
+    });
+}
